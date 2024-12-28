68cb1b3844007f2d52e014124255b9736b5247b7
Alexander0798 <rav89515139989@yandex.ru>
2024-12-28 16:42:56 +0300

    worked parser

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ void (async () => {
             return targetelemet
         }, region)
     }
+    // ждём загрузки после измененния региона 
+    await setTimeout(2000)
     //  собирает необходимые данные
     const data = await page.evaluate(() => {
         const priceOld = (document.querySelector('#__next > div.FeatureAppLayoutBase_layout__0HSBo.FeatureAppLayoutBase_hideBannerMobile__97CUm.FeatureAppLayoutBase_hideBannerTablet__dCMoJ.FeatureAppLayoutBase_hideBannerDesktop__gPdf1 > main > div:nth-child(3) > div > div.ProductPage_informationBlock__vDYCH > div.ProductPage_desktopBuy__cyRrC > div > div > div > div.PriceInfo_root__GX9Xp > div > span.Price_price__QzA8L.Price_size_XS__ESEhJ.Price_role_old__r1uT1').innerText).split(' ')[0];
